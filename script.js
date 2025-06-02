6f6666b1c4155767193a5d67cd33c886b58ed82d
SidraShahid0510 <sidrashahid0510@gmail.com>
2025-06-02 12:02:06 +0200

    portfolio
    designed single page portfolio website.
    added three projects which are Yumm Stories, Game hub and Community science museum.
    added images and some mockups to make this website look professional.
    added animation and transition on button and sections.

---
+++ script.js
@@ -0,0 +1,36 @@
+/*hamburger menu*/
+const menuIcon = document.querySelector(".menu-icon");
+const openMenu = menuIcon.querySelector(".open-menu");
+const closeMenu = menuIcon.querySelector(".close-menu");
+const navLinks = document.querySelector(".nav-links");
+
+closeMenu.style.display = "none";
+// Open mobile menu
+openMenu.addEventListener("click", () => {
+  navLinks.classList.add("active");
+  openMenu.style.display = "none";
+  closeMenu.style.display = "block";
+});
+// Close mobile menu
+closeMenu.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  openMenu.style.display = "block";
+  closeMenu.style.display = "none";
+});
+window.addEventListener("scroll", revealOnScroll);
+
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      el.classList.add("active");
+    }
+  });
+}
+
+revealOnScroll();
